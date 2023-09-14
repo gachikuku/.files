c0ee772a3f98a0a0feac0772002ae824c1d348e3
Alnoman Kamil <noman@kamil.gr>
2023-09-14 15:46:44 +0300

    Playing with colours or colors whatever

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -2,6 +2,11 @@ require('rose-pine').setup({
     variant = 'dawn',
     light_variant = 'main',
     disable_background = true,
+    highlight_groups = {
+        -- Consistent with jcs theme
+		StatusLine = { bg = "#444444", fg = "#f4f0dc"  },
+		StatusLineNC = { bg = "#444444", fg = "#f4f0dc", blend = 33  }
+	},
 })
 
 vim.cmd('colorscheme rose-pine')
