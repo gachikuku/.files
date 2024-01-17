612b10b0aa42b67134a0f4b1ec55f328cbf95ed6
Alnoman Kamil <noman@kamil.gr>
2024-01-17 08:54:53 +0200

    kanagawa i like it a lot lotus

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,24 @@
 require("config.set")
 require("config.remap")
 
+local augroup = vim.api.nvim_create_augroup
+local gachikukugroup = augroup('gachikuku', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+-- Bootstraping lazyvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
@@ -20,3 +38,7 @@ require("lazy").setup({
 	{import = "plugins"}, 
 })
 
+-- clearing up the netrw view
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
