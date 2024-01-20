b3df84b1b49e8b54dfe192e2953a5a7ce210b532
Alnoman Kamil <noman@kamil.gr>
2024-01-20 15:13:14 +0200

    don't need zenmode I AIN'T A SNOWFLAKE!!!

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,6 @@ return {
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>ht', builtin.help_tags, {})
-        -- vim.keymap.set('n', '<leader>df', builtin.find_files{search_dirs = {"hey"}}, {search_dirs={"hey"}})
+        -- How to telescope back to nvim dotfiles with "dt" ?
     end,
 }
