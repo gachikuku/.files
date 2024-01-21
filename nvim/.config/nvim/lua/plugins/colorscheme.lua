55c96dfee4b5ab96a83f786693f930fc87a4eb95
Alnoman Kamil <noman@kamil.gr>
2024-01-22 00:04:49 +0200

    requiring loads the file even if lazy = true

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
     "rebelot/kanagawa.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
         require("kanagawa").setup({
+            lazy = false, -- make sure we load this during startup if it is your main colorscheme
+            priority = 1000, -- make sure to load this before all the other start plugins
             transparent = true,
             colors = {
                 theme = {
