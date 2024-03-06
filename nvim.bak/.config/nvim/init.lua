ad3baa03e759a591b37dfffaf2657fc714ac47ae
Alnoman Kamil <noman@kamil.gr>
2024-03-06 23:17:21 +0200

    updated neovimrc

---
+++ nvim.bak/.config/nvim/init.lua
@@ -0,0 +1,41 @@
+---Is there a plugin or a program that whathes for remap conflicts?
+
+require("config.set")
+require("config.keymap")
+
+local augroup = vim.api.nvim_create_augroup
+local gachikukugroup = augroup('gachikuku', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+-- Bootstraping lazyvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	import = "plugins",
+})
