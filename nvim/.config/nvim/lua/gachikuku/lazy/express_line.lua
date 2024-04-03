a3b206e05bd97249a93e78fc03a8afa2b9f80ff6
Alnoman Kamil <noman@kamil.gr>
2024-04-03 18:38:54 +0300

    luaexpress

---
+++ nvim/.config/nvim/lua/gachikuku/lazy/express_line.lua
@@ -0,0 +1,10 @@
+return {
+    "tjdevries/express_line.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    config = function()
+        require('el').setup {}
+    end,
+}
+
--- nvim/.config/nvim/lua/gachikuku/set.lua
+++ nvim/.config/nvim/lua/gachikuku/set.lua
@@ -28,3 +28,6 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+vim.api.nvim_set_hl(0, "CmpItemKindCody", { fg = "Red" })
+
