c6abe8610b0cd80e028b31489c155382660d099d
Alnoman Kamil <noman@kamil.gr>
2024-07-18 17:17:52 +0300

    pasting

--- nvim/.config/nvim/lua/gachikuku/remap.lua
+++ nvim/.config/nvim/lua/gachikuku/remap.lua
@@ -43,3 +43,5 @@ vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+vim.keymap.set('n', '<leader>pp', ':set paste<CR>"+p:set nopaste<CR>', { silent = true })
--- nvim/.config/nvim/lua/gachikuku/set.lua
+++ nvim/.config/nvim/lua/gachikuku/set.lua
@@ -8,7 +8,6 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
@@ -30,4 +29,3 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
 vim.api.nvim_set_hl(0, "CmpItemKindCody", { fg = "Red" })
-
