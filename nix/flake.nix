828ece007cd183454fe98cbc7d1c7ded9e9a899d
Alnoman Kamil <noman@kamil.gr>
2024-12-04 23:13:20 +0200

    nix!

---
+++ nix/flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "nix-darwin";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ 
+	  pkgs.vim
+	  pkgs.gnupg
+	  pkgs.coreutils-full
+	  pkgs.gopass
+	  pkgs.tailscale
+	  pkgs.aerc
+	  pkgs.senpai
+	  pkgs.stow
+        ];
+
+      system.defaults = {
+        dock.autohide = true;
+      };
+
+      system.keyboard = {
+        enableKeyMapping = true;
+        remapCapsLockToControl = true;
+      };
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#gabbass-MacBook-Pro
+    darwinConfigurations."gachimacos" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+
+    # NOT DEFAULT: Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."gachimacos".pkgs;
+  };
+}
