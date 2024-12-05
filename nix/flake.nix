b412b70c6230d9180e11b893a543c99c7ce003ff
Alnoman Kamil <noman@kamil.gr>
2024-12-05 18:26:00 +0200

    System settings!

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,72 +1,82 @@
 {
-  description = "nix-darwin";
+	description = "nix-darwin";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-  };
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+		nix-darwin.url = "github:LnL7/nix-darwin";
+		nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+	};
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages = with pkgs;
-        [ 
-	  aerc
-	  coreutils-full
-	  gnupg
-	  gopass
-	  neovim
-	  nodejs_23
-	  senpai
-	  stow
-	  tailscale
-	  tmux
-	  vim
-          kitty
-        ];
+	outputs = inputs@{ self, nix-darwin, nixpkgs }:
+		let
+			configuration = { pkgs, ... }: {
+				# List packages installed in system profile. To search by name, run:
+				# $ nix-env -qaP | grep wget
+				environment.systemPackages = with pkgs;
+					[ 
+						aerc
+						coreutils-full
+						gnupg
+						gopass
+						kitty
+						neovim
+						nodejs_23
+						senpai
+						stow
+						tailscale
+						tmux
+						vim
+						wget
+					];
 
-      system.keyboard = {
-        enableKeyMapping = true;
-        remapCapsLockToControl = true;
-      };
+				system.keyboard = {
+					enableKeyMapping = true;
+					remapCapsLockToControl = true;
+				};
 
-      system.defaults = {
-        dock.autohide = true;
-		dock.orientation = "left";
-		NSGlobalDomain.AppleShowAllExtensions = true;
-		NSGlobalDomain.AppleICUForce24HourTime = true;
-		menuExtraClock.ShowAMPM = false;
-		menuExtraClock.ShowDayOfWeek = false;
-      };
-	  
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+				system.defaults = {
+					NSGlobalDomain."com.apple.sound.beep.feedback" = 0;
+					NSGlobalDomain.AppleICUForce24HourTime = true;
+					NSGlobalDomain.AppleShowAllExtensions = true;
+					NSGlobalDomain.InitialKeyRepeat = 0;
+					NSGlobalDomain.KeyRepeat = 2;
+					NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;
+					NSGlobalDomain.NSAutomaticDashSubstitutionEnabled = false;
+					NSGlobalDomain.NSAutomaticInlinePredictionEnabled = false;
+					NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
+					controlcenter.Sound = true;
+					dock.autohide = true;
+					dock.orientation = "left";
+					menuExtraClock.ShowAMPM = false;
+					menuExtraClock.ShowDayOfWeek = false;
+					screencapture.location = "~/Screenshots";
+				};
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+				# Necessary for using flakes on this system.
+				nix.settings.experimental-features = "nix-command flakes";
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+				# Enable alternative shell support in nix-darwin.
+				# programs.fish.enable = true;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
+				# Set Git commit hash for darwin-version.
+				system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#gabbass-MacBook-Pro
-    darwinConfigurations."gachimacos" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
+				# Used for backwards compatibility, please read the changelog before changing.
+				# $ darwin-rebuild changelog
+				system.stateVersion = 5;
 
-    # NOT DEFAULT: Expose the package set, including overlays, for convenience.
-    # darwinPackages = self.darwinConfigurations."gachimacos".pkgs;
-  };
+				# The platform the configuration will be used on.
+				nixpkgs.hostPlatform = "aarch64-darwin";
+			};
+		in
+			{
+			# Build darwin flake using:
+			# $ darwin-rebuild build --flake .#gabbass-MacBook-Pro
+			darwinConfigurations."gachimacos" = nix-darwin.lib.darwinSystem {
+				modules = [ configuration ];
+			};
+
+			# NOT DEFAULT: Expose the package set, including overlays, for convenience.
+			# darwinPackages = self.darwinConfigurations."gachimacos".pkgs;
+		};
 }
