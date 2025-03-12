038ce5811c6c3def38e2af52afbbabbcd570a29b
Alnoman Kamil <noman@kamil.gr>
2025-03-12 02:24:37 +0200

    flexoki-dark

--- nvim/.config/nvim/lua/gachikuku/lazy/colors.lua
+++ nvim/.config/nvim/lua/gachikuku/lazy/colors.lua
@@ -1,15 +1,16 @@
 function ColorMyPencils(color)
-	color = color or "solarized-osaka"
+	color = color or "flexoki-dark"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	--vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
 
 end
 
 return {
 
-    "craftzdog/solarized-osaka.nvim",
+    "kepano/flexoki-neovim",
 
     lazy = false,
     priority = 1000,
