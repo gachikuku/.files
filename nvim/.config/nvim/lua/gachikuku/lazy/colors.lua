36e9a7d9355f5e18e617967fba91cacf8bbcc9c3
Alnoman Kamil <noman@kamil.gr>
2026-01-12 14:56:51 +0200

    light theme

--- nvim/.config/nvim/lua/gachikuku/lazy/colors.lua
+++ nvim/.config/nvim/lua/gachikuku/lazy/colors.lua
@@ -1,11 +1,9 @@
 function ColorMyPencils(color)
-    color = color or "flexoki-dark"
+    color = color or "flexoki-light"
     vim.cmd.colorscheme(color)
-
     vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
     -- vim.api.nvim_set_hl(0, "LineNr", { bg = "NONE" })
-
     vim.api.nvim_set_hl(0, "StatusLine", { fg = "grey", bg = "NONE" })
     vim.api.nvim_set_hl(0, "StatusLineNC", { fg = "grey", bg = "NONE" })
     vim.api.nvim_set_hl(0, "MsgArea", { fg = "grey", bg = "NONE" })
@@ -13,9 +11,16 @@ end
 
 return {
     "kepano/flexoki-neovim",
+    name = "flexoki",
     lazy = false,
     priority = 1000,
     config = function()
-        ColorMyPencils()
+        require("flexoki").setup({
+            variant = "light", -- This ensures light variant
+            styles = {
+                transparency = false,
+            },
+        })
+        ColorMyPencils("flexoki-light")
     end
 }
