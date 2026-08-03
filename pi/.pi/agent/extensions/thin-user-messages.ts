7167f6592a80787b760532645af1b1492b9e0532
Alnoman Kamil <noman@kamil.gr>
2026-08-03 06:23:33 +0300

    save

---
+++ pi/.pi/agent/extensions/thin-user-messages.ts
@@ -0,0 +1,28 @@
+import { UserMessageComponent, type ExtensionAPI } from "@earendil-works/pi-coding-agent";
+import { Box } from "@earendil-works/pi-tui";
+
+const PATCH_MARK = "__thinUserMessagesPatched";
+
+type UserMessagePrototype = {
+	[PATCH_MARK]?: boolean;
+	rebuild(this: UserMessageComponent): void;
+};
+
+type MutableBox = Box & { paddingY: number };
+
+export default function (_pi: ExtensionAPI) {
+	const prototype = UserMessageComponent.prototype as unknown as UserMessagePrototype;
+	if (prototype[PATCH_MARK]) return;
+
+	const originalRebuild = prototype.rebuild;
+	prototype.rebuild = function () {
+		originalRebuild.call(this);
+
+		const contentBox = this.children[0];
+		if (contentBox instanceof Box) {
+			(contentBox as MutableBox).paddingY = 0;
+			contentBox.invalidate();
+		}
+	};
+	prototype[PATCH_MARK] = true;
+}
